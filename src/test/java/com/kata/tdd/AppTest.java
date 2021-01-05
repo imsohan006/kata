ba18ee8e5059012f1a19ac7778727a1be6da4d4e
imsohan006 <sohanyadav0011@gmail.com>
2021-01-05 20:03:54 +0530

    initial repo creation

---
+++ src/test/java/com/kata/tdd/AppTest.java
@@ -0,0 +1,21 @@
+package com.kata.tdd;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+	/**
+     * Rigorous Test :-)
+     * @throws InvalidNumberException 
+     */
+	@Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
